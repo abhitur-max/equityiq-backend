3b826296c3e937cf48a3cd7a8799b6f21d4440a1
abhitur-max <abhitur@gmail.com>
2026-03-04 15:23:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -26,8 +26,8 @@ app = FastAPI(title="EquityIQ Backend", version="1.0.0")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
+    allow_credentials=False,
+    allow_methods=["GET","POST","OPTIONS"],
     allow_headers=["*"],
 )
 
